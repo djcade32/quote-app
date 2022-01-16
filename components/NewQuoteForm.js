cca2421cea89f7eef0c79f7a7e28f9d96012456d
Norman <norman_cade32@yahoo.com>
2022-01-16 00:40:31 -0500

    Can create custom quotes.

---
+++ components/NewQuoteForm.js
@@ -0,0 +1,154 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Button } from "react-native-elements";
+import { useForm, Controller } from "react-hook-form";
+import Colors from "../constants/Colors";
+
+const NewQuoteForm = (props) => {
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      text: "",
+      author: "",
+    },
+  });
+
+  function onSubmit(data) {
+    reset({
+      text: "",
+      author: "",
+    });
+    console.log(data);
+    props.createQuoteHandler(data);
+  }
+
+  return (
+    <LinearGradient
+      colors={[Colors.cardBgColor, Colors.backgroundColor]}
+      style={{
+        borderRadius: 15,
+      }}
+    >
+      <View style={styles.formContainer}>
+        <Text style={styles.formTitle}>Create New Quote</Text>
+        <View>
+          <Text style={styles.label}>Quote</Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={[styles.textArea, errors.text && styles.errorInput]}
+                multiline={true}
+                numberOfLines={3}
+                placeholder="Type quote"
+                placeholderTextColor={"grey"}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="text"
+          />
+          {errors.text && (
+            <Text style={styles.errorMessage}>This is required.</Text>
+          )}
+        </View>
+        <View>
+          <Text style={styles.label}>Author</Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+              maxLength: 100,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={[styles.input, errors.author && styles.errorInput]}
+                placeholder="Who said it?"
+                placeholderTextColor={"grey"}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="author"
+          />
+          {errors.author && (
+            <Text style={styles.errorMessage}>This is required.</Text>
+          )}
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button title="Create" onPress={handleSubmit(onSubmit)} />
+        </View>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default NewQuoteForm;
+
+const styles = StyleSheet.create({
+  formContainer: {
+    height: "90%",
+    justifyContent: "space-around",
+    padding: 20,
+  },
+  formTitle: {
+    color: "white",
+    textAlign: "center",
+    fontFamily: "open-sans-bold",
+    fontSize: 30,
+  },
+  label: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 25,
+    marginBottom: 5,
+  },
+  textArea: {
+    color: "black",
+    fontFamily: "open-sans",
+    fontSize: 20,
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 5,
+    height: 85,
+    width: "100%",
+  },
+  input: {
+    color: "black",
+    fontFamily: "open-sans",
+    fontSize: 20,
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 5,
+    height: 40,
+  },
+  errorInput: {
+    borderColor: Colors.heartColor,
+    borderWidth: 1.5,
+  },
+  errorMessage: {
+    marginTop: 5,
+    color: Colors.heartColor,
+  },
+  buttonWrapper: {
+    borderRadius: 15,
+    overflow: "hidden",
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 3.84,
+  },
+});
--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,95 +1,48 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   StyleSheet,
-  Text,
   View,
-  TextInput,
-  Button,
   SafeAreaView,
   Keyboard,
   TouchableWithoutFeedback,
+  Alert,
+  Text,
 } from "react-native";
-import Colors from "../constants/Colors";
-import { useForm, Controller } from "react-hook-form";
-import { LinearGradient } from "expo-linear-gradient";
+import { Overlay, Button } from "react-native-elements";
 
-const CreateScreen = (props) => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      quote: "",
-      author: "",
-    },
-  });
-  const onSubmit = (data) => console.log(data);
+import NewQuoteForm from "../components/NewQuoteForm";
+import Colors from "../constants/Colors";
+import { quotesActions } from "../store/quotesSlice";
+
+const CreateScreen = () => {
+  const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+  function createQuoteHandler(data) {
+    dispatch(quotesActions.addQuote(data));
+    toggleOverlay();
+    // Alert.alert("New Quote", "Sucess!", [
+    //   { text: "OK", onPress: () => console.log("OK Pressed") },
+    // ]);
+  }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
         <SafeAreaView style={{ width: "84%", maxWidth: "84%" }}>
-          <LinearGradient
-            colors={[Colors.cardBgColor, Colors.backgroundColor]}
-            style={{
-              borderRadius: 15,
-            }}
-          >
-            <View style={styles.formContainer}>
-              <Text style={styles.formTitle}>Create New Quote</Text>
-              <View>
-                <Text style={styles.label}>Quote</Text>
-                <Controller
-                  control={control}
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      style={styles.textArea}
-                      multiline={true}
-                      numberOfLines={3}
-                      placeholder="Type quote"
-                      placeholderTextColor={"grey"}
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={value}
-                    />
-                  )}
-                  name="quote"
-                />
-                {errors.quote && (
-                  <Text style={styles.errorMessage}>This is required.</Text>
-                )}
-              </View>
-              <View>
-                <Text style={styles.label}>Author</Text>
-                <Controller
-                  control={control}
-                  rules={{
-                    required: true,
-                    maxLength: 100,
-                  }}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                      style={styles.input}
-                      placeholder="Who said it?"
-                      placeholderTextColor={"grey"}
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={value}
-                    />
-                  )}
-                  name="author"
-                />
-                {errors.author && (
-                  <Text style={styles.errorMessage}>This is required.</Text>
-                )}
-              </View>
-              <Button title="Create" onPress={handleSubmit(onSubmit)} />
-            </View>
-          </LinearGradient>
+          <NewQuoteForm createQuoteHandler={createQuoteHandler} />
+          <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+            <Text style={styles.textPrimary}>SUCCESS!</Text>
+            <Text style={styles.textSecondary}>New quote created.</Text>
+            <Button
+              buttonStyle={styles.button}
+              title="Okay"
+              onPress={toggleOverlay}
+            />
+          </Overlay>
         </SafeAreaView>
       </View>
     </TouchableWithoutFeedback>
@@ -105,43 +58,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.backgroundColor,
   },
-  formContainer: {
-    height: "90%",
-    justifyContent: "space-around",
-    padding: 20,
+  button: {
+    margin: 10,
   },
-  formTitle: {
-    color: "white",
+  textPrimary: {
+    marginVertical: 20,
     textAlign: "center",
-    fontFamily: "open-sans-bold",
-    fontSize: 30,
-  },
-  label: {
-    color: "white",
-    fontFamily: "open-sans",
-    fontSize: 25,
-    marginBottom: 5,
-  },
-  textArea: {
-    color: "black",
-    fontFamily: "open-sans",
     fontSize: 20,
-    backgroundColor: "white",
-    borderRadius: 5,
-    padding: 5,
-    height: 85,
-    width: "100%",
   },
-  input: {
-    color: "black",
-    fontFamily: "open-sans",
-    fontSize: 20,
-    backgroundColor: "white",
-    borderRadius: 5,
-    padding: 5,
-    height: 40,
-  },
-  errorMessage: {
-    color: Colors.heartColor,
+  textSecondary: {
+    marginBottom: 10,
+    textAlign: "center",
+    fontSize: 17,
   },
 });
